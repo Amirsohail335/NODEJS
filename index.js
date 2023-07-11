3aa14bc18fcec65d8bbedcea411c1eab538995ef
Amirsohail335 <amirsohail335@gmail.com>
2023-07-11 18:26:21 +0530

    committed

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const http = require("http");
+// const http = require("http");
+import http from "http";
+import gfName from "./feature.js";
+
 
 const server = http.createServer((req , res) => {
-    console.log(req.url)
+    if(req.url === "/about"){
+        res.end("<h1>About Page</h1>");
+    }
+
+    else if(req.url === "/"){
+        res.end("<h1>Home page</h1>");
+    }
+
+    else if(req.url === "/contact"){
+        res.end("<h1>Contact Page</h1>");
+    }
+
+    else if(req.url === "/user"){
+        res.end("<h1>User Page</h1>");
+    }
+    else{
+        res.end("<h1>Page not found</h1>")
+    }
+   
 })
 
 server.listen(5000 ,()=> {
