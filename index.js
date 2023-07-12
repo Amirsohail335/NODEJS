596a3178d45616f258686bdbf8f84ca1ddf60ad4
Amirsohail335 <amirsohail335@gmail.com>
2023-07-12 10:36:51 +0530

    committed

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
-// const http = require("http");
-import http from "http";
-import gfName from "./feature.js";
+// // const http = require("http");
+// import http from "http";
+// import gfName from "./feature.js";
 
 
-const server = http.createServer((req , res) => {
-    if(req.url === "/about"){
-        res.end("<h1>About Page</h1>");
-    }
+// const server = http.createServer((req , res) => {
+//     if(req.url === "/about"){
+//         res.end("<h1>About Page</h1>");
+//     }
 
-    else if(req.url === "/"){
-        res.end("<h1>Home page</h1>");
-    }
+//     else if(req.url === "/"){
+//         res.end("<h1>Home page</h1>");
+//     }
 
-    else if(req.url === "/contact"){
-        res.end("<h1>Contact Page</h1>");
-    }
+//     else if(req.url === "/contact"){
+//         res.end("<h1>Contact Page</h1>");
+//     }
 
-    else if(req.url === "/user"){
-        res.end("<h1>User Page</h1>");
-    }
-    else{
-        res.end("<h1>Page not found</h1>")
-    }
+//     else if(req.url === "/user"){
+//         res.end("<h1>User Page</h1>");
+//     }
+//     else{
+//         res.end("<h1>Page not found</h1>")
+//     }
    
+// })
+
+// server.listen(5000 ,()=> {
+//     console.log("Server is working")
+// })
+
+import express from "express";
+
+const app = express();
+
+app.get("/",(req, res)=>{
+    // res.send("hi")
+    res.json({
+        success:true,
+        products:[],
+    })
 })
 
-server.listen(5000 ,()=> {
+app.listen(5000 , ()=>{
     console.log("Server is working")
 })
-
