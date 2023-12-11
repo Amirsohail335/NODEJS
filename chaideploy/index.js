724c28764791974d0c9a50465d00dd1acc0c044d
Amirsohail335 <amirsohail335@gmail.com>
2023-12-11 12:26:01 +0530

    committed

--- chaideploy/index.js
+++ chaideploy/index.js
@@ -18,6 +18,10 @@ app.get("/login", (req, res) => {
   console.log("<h1>Login Page</h1>");
 });
 
+app.get('/signup',(req , res)=>{
+  console.log("<h1>Signup Page</h1>");
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
